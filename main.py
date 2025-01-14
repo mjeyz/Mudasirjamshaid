bd0534e2e5f2fab0155790a01759c105ed597ee8
Mudasir Jamshaid <thisismjeyz@gmail.com>
2025-01-14 09:50:18 +0500

    Day 47 Instant Pot Project
    this project is interesting

--- main.py
+++ main.py
@@ -1,86 +1,37 @@
-from tkinter import *
-
-#Creating a new window and configurations
-window = Tk()
-window.title("Widget Examples")
-window.minsize(width=500, height=500)
-
-#Labels
-label = Label(text="This is old text")
-label.config(text="This is new text")
-label.pack()
-
-#Buttons
-def action():
-    print("Do something")
-
-#calls action() when pressed
-button = Button(text="Click Me", command=action)
-button.pack()
-
-#Entries
-entry = Entry(width=30)
-#Add some text to begin with
-entry.insert(END, string="Some text to begin with.")
-#Gets text in entry
-print(entry.get())
-entry.pack()
-
-#Text
-text = Text(height=5, width=30)
-#Puts cursor in textbox.
-text.focus()
-#Adds some text to begin with.
-text.insert(END, "Example of multi-line text entry.")
-#Get's current value in textbox at line 1, character 0
-print(text.get("1.0", END))
-text.pack()
-
-#Spinbox
-def spinbox_used():
-    #gets the current value in spinbox.
-    print(spinbox.get())
-spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
-spinbox.pack()
-
-#Scale
-#Called with current scale value.
-def scale_used(value):
-    print(value)
-scale = Scale(from_=0, to=100, command=scale_used)
-scale.pack()
-
-#Checkbutton
-def checkbutton_used():
-    #Prints 1 if On button checked, otherwise 0.
-    print(checked_state.get())
-#variable to hold on to checked state, 0 is off, 1 is on.
-checked_state = IntVar()
-checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
-checked_state.get()
-checkbutton.pack()
-
-#Radiobutton
-def radio_used():
-    print(radio_state.get())
-#Variable to hold on to which radio button value is checked.
-radio_state = IntVar()
-radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
-radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
-radiobutton1.pack()
-radiobutton2.pack()
+from bs4 import BeautifulSoup
+import requests
+import smtplib
 
 
-#Listbox
-def listbox_used(event):
-    # Gets current selection from listbox
-    print(listbox.get(listbox.curselection()))
+practice_url = "https://appbrewery.github.io/instant_pot/"
+live_url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+PASSWORD = "apul vxon fmai kxgx"
+MY_EMAIL = "thisismjeyz@gmail.com"
+TO_ADDRESS= "mudasirjamshaid0@gmail.com"
 
-listbox = Listbox(height=4)
-fruits = ["Apple", "Pear", "Orange", "Banana"]
-for item in fruits:
-    listbox.insert(fruits.index(item), item)
-listbox.bind("<<ListboxSelect>>", listbox_used)
-listbox.pack()
-window.mainloop()
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "Accept-Language": "en-GB,en;q=0.8",
+}
 
+response = requests.get(practice_url)
+
+soup = BeautifulSoup(response.content, "html.parser")
+price = soup.find(class_="a-offscreen").get_text()
+price_without_currency = price.split("$")[1]
+
+price_as_float = float(price_without_currency)
+
+if price_as_float < 100:
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL,
+                                to_addrs= TO_ADDRESS,
+                                msg=f"subject:Amazon Price Alert \n\n Today price of your product is  {price_as_float}."
+                                    f" Go and buy now.")
+    except smtplib.SMTPException as e:
+        print(f"An smtp error is happening and error is {e}")
+    else:
+        print(f"Message sent to {TO_ADDRESS} successfully.")
